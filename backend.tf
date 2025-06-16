f431544b74ba9c519ec673582c5ee880ad67edfb
Hamlet A. Rodriguez <hamletrp@gmail.com>
2025-06-16 14:32:49 -0400

    update ft branch remote

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-3cbash"
-    key            = "dev/terraform-eks-cluster-lab-14.tfstate"
+    key            = "dev/terraform-eks-cluster-lab-13.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
